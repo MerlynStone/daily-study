f7c3faf24488257c1798a19b012e70fdabd880bb
malin <malin@kedacom.com>
2021-12-27 13:50:13 +0800

    添加注释

--- algorithm/1-chain/3chain.js
+++ algorithm/1-chain/3chain.js
@@ -1,10 +1,9 @@
+// 通过两个数据记录相应坐标索引值来实现链表的效果
 var data=new Array(10);
 var next=new Array(10);
-console.log(data[1])
 function add(index,point,val) {
     next[index]=point;
     data[point]=val
-    
 }
 let head=3;
 data[3]=0;
@@ -12,6 +11,7 @@ add(3,5,1)
 add(5,2,2)
 add(2,7,3)
 add(7,9,100)
+// 链表上的节点值依次为0—>1->2->3->100
 console.log(data)
 console.log(next)
 let p=head;
