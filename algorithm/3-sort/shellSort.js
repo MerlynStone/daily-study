f81aeb36768b66702800e7e329857089c36967d0
merlyn.pc <291186465@qq.com>
2021-12-28 22:49:26 +0800

    希尔排序

---
+++ algorithm/3-sort/shellSort.js
@@ -0,0 +1,24 @@
+// 希尔排序
+let arr = [1, 1, 35, 5, 6, 5, 56, 35]
+console.log(shellSort(arr))
+function shellSort(arr) {
+    let len = arr.length
+    let gap = Math.floor(len / 2)
+    for (gap; gap > 0; gap = Math.floor(gap / 2)) {
+        for (let i = gap; i < len; i++) {
+            let tem = arr[i]
+            for (var j = i - gap; j >= 0 && arr[j] > tem; j -= gap) {
+                arr[j + gap] = arr[i]
+            }
+            arr[j + gap] = tem
+
+        }
+    }
+    return arr
+}
+// 交换
+function swap(array, i, j) {
+    let tem = array[i];
+    array[i] = array[j]
+    array[j] = tem
+}
